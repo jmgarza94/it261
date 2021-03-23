e2da14c92a9c29ebe79b3c2cd018ac590e9fa0ab
Joey Garza <47196145+jmgarza94@users.noreply.github.com>
2021-03-23 15:08:10 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>IT261 Portal Page</title>
+<link rel="stylesheet" href="css/main.css"/>
+<link rel="stylesheet" href="css/nav.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+</head>
+
+<body>
+    <div class="wrapper">
+    <header>
+    <div class="banner">
+    <h1>Joey's Portal Page</h1>
+    </div>
+    <div class="navbar">
+    <a href="index.php">Home</a>
+    <div class="dropdown">
+    <button class="dropbtn">Assignments 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="website/daily.php">Daily Switch</a>
+      <a href="weeks/week4/adder-right.php">Adder/Troubleshooting</a>
+      <a href="weeks/week5/calculator.php">Calculator.php</a>
+      <a href="weeks/week5/calculator-days.php">Calculator-days.php</a>
+      <a href="weeks/week5/calculator-days-errors.php">Calculator-days-errors.php</a>
+      <a href="weeks/week5/calculator-days-errors-sticky.php">Calculator-days-errors-sticky.php</a>
+    </div>
+    </div> 
+    <div class="dropdown">
+    <button class="dropbtn">Weekly 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="weeks/week2/week2.php">Week 2 Assignments</a>
+      <a href="weeks/week3/week3.php">Week 3 Assignments</a>
+      <a href="weeks/week4/week4.php">Week 4 Assignments</a>
+      <a href="weeks/week5/week5.php">Week 5 Assignments</a>
+      <a href="weeks/week6/week6.php">Week 6 Assignments</a>
+      <a href="weeks/week7/week7.php">Week 7 Assignments</a>
+      <a href="weeks/week8/week8.php">Week 8 Assignments</a>
+      <a href="weeks/week9/week9.php">Week 9 Assignments</a>
+    </div>
+    </div>
+    <a href="website/daily.php">Daily</a> 
+    <a href="website/index.php">Website Folder</a> 
+    </div>
+    </header>
+    <main>
+        <h1>About Me</h1>
+            <p>Hello! My name is Josele (Joey) Garza and I am currently in the Web Development Certificate program at Seattle Central.
+             After finishing the program I hope to find a job in the field in Chicago to be close to family again.
+            I am leaning more towards the front-end design rather than back-end coding. I plan to build up my portfolio with some freelance work before pursuing a job on a web dev team.
+            </p>
+            <br>
+            <p>
+                Outside of Web Development, I work full time at a busy brunch restaurant here in Seattle as the assistant manager and server. They have taken care of me for the time I've been in Seattle, and have made sure all of their employees
+                are taken care of throughout this COVID pandemic. I plan to keep working here and paying my way through school until I finish the program. 
+                In my free time I train as an amateur boxer at Seattle Boxing Gym, with hope to eventually be able to coach people one day. I also enjoy playing video games on different platforms, and spending time with my friends.
+            </p>
+    </main>
+
+    <aside>
+        <!-- <h3>Me!</h3> -->
+        <img class="self-image" src="images/jboxing.jpg" alt="" /> 
+        
+      <h3>MAMP - HW1</h3>
+      
+      <a href="images/mamp_install.png">MAMP Install Screenshot</a>
+      <br>
+      <a href="images/mamp_errors.png">MAMP errors enabled</a>
+
+      <h3>Adminer login page screenshot</h3>
+      <a href="images/adminer.png">Adminer-4.8.0 login screenshot</a>
+    </aside>
+
+
+    <footer>   
+     <p><small>&copy; 2021 by Joey Garza, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>     
+    </footer>
+</div>
+
+</body>
+</html>
